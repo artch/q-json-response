3b7eca282f09b272c0c3810bf0bdeeb401250199
Artem Chivchalov <chivchalov@gmail.com>
2016-07-25 11:48:23 +0300

    Add next()

--- q-json-response.js
+++ q-json-response.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 
 module.exports = function(fn) {
 
-	return function(request, response) {
+	return function(request, response, next) {
 		q.when({})
 
 			.then(function() {
@@ -11,12 +11,14 @@ module.exports = function(fn) {
 			})
 			.then(function(data) {
 				response.json(_.extend({ok: 1}, data));
+				next();
 			})
 			.catch(function(error) {
 				response.json({error: "" + error});
 				if(error.stack) {
 					console.error(error.stack);
 				}
+				next();
 			});
     }
 };
